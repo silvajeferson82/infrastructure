680873098de3a3a5c2c16dcf1cbb1097a8223f4e
Jeferson Silva <jefersonsilva.contato@gmail.com>
2026-07-25 02:28:41 -0300

    refactor: migrate s3 bucket website configuration to dedicated resource and update access/ownership settings

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -9,14 +9,21 @@ variable "bucket_name" {
 resource "aws_s3_bucket" "static_site_bucket" {
   bucket = "static-site-${var.bucket_name}"
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
   tags = {
     Name        = "static-site-${var.bucket_name}"
     Environment = "production"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
   }
 }
 
@@ -26,14 +33,14 @@ resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
-  restrict_public_policies = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -45,5 +52,6 @@ resource "aws_s3_bucket_acl" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
 }
+
 
 
